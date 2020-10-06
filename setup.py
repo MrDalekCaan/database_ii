5cec3ab2dc276d15f003e4e6e8fd912ad447404b
Dalek <iamabaddoctor@gmail.com>
2020-10-06 18:49:58 +0800

    try to work on windows

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ main()
 user_id = eu.register("flor", "123456")
 eu.e_shop_cursor.execute(f"INSERT INTO user(user_name, user_id, password, type) VALUES('admin', '000000', '000000', '0000')")
 with open("defaultuser.json", 'w') as file:
-	json.dump(file, {"customer": {
+	json.dump({"customer": {
 			"username": "flor",
 			"password": "123456",
 			"userid": user_id
@@ -17,5 +17,5 @@ with open("defaultuser.json", 'w') as file:
 			"password": "000000",
 			"userid": "000000"
 		}
-	})
+	},file)
 
