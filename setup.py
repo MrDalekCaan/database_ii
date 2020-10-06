b6300ad84548e1966cd2374e0828b058ab3b9d3f
Dalek <iamabaddoctor@gmail.com>
2020-10-06 18:27:17 +0800

    fix setup

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-import bulid_database
+from build_database import main
 import e_shop_user as eu
 import json
 
 
-bulid_database.main()
+main()
 user_id = eu.register("flor", "123456")
 eu.e_shop_cursor.execute(f"INSERT INTO user(user_name, user_id, password, type) VALUES('admin', '000000', '000000', '0000')")
 with open("defaultuser.json") as file:
 	json.dump(file, {"customer": {
 			"username": "flor",
-			"password": "123456"
+			"password": "123456",
 			"userid": user_id
 		},
 		"admin": {
